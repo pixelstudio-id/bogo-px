8ec7e5076b86098cd87a8ee9d845b73b2d30c90f
Henner S <henner.renardi@gmail.com>
2025-06-14 16:10:23 +0700

    4.8.6
    - Fixed the URL switcher not working when it's dynamic or doesn't have trailing slash.
    - Fixed the URL switcher not knowing the homepage on other locale page.

--- custom/global-link-groups.php
+++ custom/global-link-groups.php
@@ -22,6 +22,15 @@ function bogo_init_global_link_groups() {
 
   $groups = [];
 
+  // get homepage ID the raw way because get_option() returns null on locale page
+  $home_id = wp_cache_get('raw_page_on_front', 'bogopx');
+  if ($home_id === false) {
+    global $wpdb;
+    $home_id = $wpdb->get_var("SELECT option_value FROM {$wpdb->options} WHERE option_name = 'page_on_front'");
+    wp_cache_set('raw_page_on_front', $home_id, 'bogopx', 86400); // 24 hour cache
+  }
+  $home_id = (int) $home_id;
+
   foreach ($posts as $p) {
     $original_post_id = get_post_meta($p->ID, '_original_post', true);
 
@@ -30,11 +39,20 @@ function bogo_init_global_link_groups() {
     }
 
     $locale = get_post_meta($p->ID, '_locale', true) ?: BOGO_DEFAULT_LOCALE;
+    $url = '';
+
+    if ($home_id === $p->ID) {
+      $url = trailingslashit(home_url());
+    } else if ($home_id == $original_post_id) { // @warn - intentionally "==" because $original_post_id is a string
+      $url = trailingslashit(home_url()) . '/' . bogo_lang_slug($locale);
+    } else {
+      $url = get_permalink($p->ID);
+    }
 
     $groups[$original_post_id][$locale] = [
       'id' => $p->ID,
       'locale' => $locale,
-      'url' => get_permalink($p),
+      'url' => $url,
       'post' => $p,
     ];
   }
@@ -80,13 +98,20 @@ function bogo_init_global_link_groups() {
  * Get the translated version of a post by URL
  */
 function bogo_localize_by_url($url) {
-  $parsed_url = parse_url($url);
+  $url_with_trailing_slash = preg_replace('/^([^?#]*[^\/?])([?#]|$)/', '$1/$2', $url);
+  $parsed_url = parse_url($url_with_trailing_slash);
 
-  // abort if doesn't have scheme (maybe a #id link)
-  if (!isset($parsed_url['scheme'])) { return null; }
+  // abort if anchor link (just #id)
+  if (!isset($parsed_url['path'])) { return null; }
 
-  // abort if doesn't have host (maybe a tel or mailto link)
-  if (!isset($parsed_url['host'])) { return null; }
+  // abort if special link like mailto: or tel: (no host but has scheme)
+  if (!isset($parsed_url['host']) && isset($parsed_url['scheme'])) { return null; }
+
+  // append domain name to dynamic URL (no scheme and host)
+  if (!isset($parsed_url['scheme']) && !isset($parsed_url['host'])) {
+    $parsed_url['scheme'] = ( ! empty( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] !== 'off' ) ? 'https' : 'http';
+    $parsed_url['host'] = $_SERVER['HTTP_HOST'];
+  }
 
   $base_url = "{$parsed_url['scheme']}://{$parsed_url['host']}";
 
@@ -101,6 +126,10 @@ function bogo_localize_by_url($url) {
   
   if (isset($parsed_url['query'])) {
     $link['url'] .= "?{$parsed_url['query']}";
+  }
+
+  if (isset($parsed_url['fragment'])) {
+    $link['url'] .= "#{$parsed_url['fragment']}";
   }
 
   return $link ?: null;
